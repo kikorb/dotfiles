6596c2d967faefa16678e2969404bf3aa750d441
Francisco Ruiz <kikorb@gmail.com>
2016-11-07 08:35:38 +0100

    Update setup_macos.sh

--- bin/setup_macos.sh
+++ bin/setup_macos.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
 
 defaults write com.apple.screencapture type jpg
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
